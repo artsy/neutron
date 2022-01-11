2f5c601216c68b1769ca1fc0a0b71df52bb7accc
Jon Allured <jon.allured@gmail.com>
2022-01-11 15:08:27 -0600

    Wire up a couple basic query fields

---
+++ app/graphql/types/article_type.rb
@@ -0,0 +1,5 @@
+module Types
+  class ArticleType < Types::BaseObject
+    field :title, String
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/api/graphql"
   end
-  post "/graphql", to: "graphql#execute"
+  post "/api/graphql", to: "graphql#execute"
   resources :articles, only: [:index, :edit, :new, :create, :update, :destroy]
   # root "home#index"
 end
